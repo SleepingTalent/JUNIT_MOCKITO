c5c56743f1d9ab8bd23a2a88152e1c387b733feb
James Fairbairn <jaybono30@hotmail.com>
2013-08-12 09:21:28 +0100

    Initial Structure Setup

--- mockito_example/src/main/java/com/fs/humanResources/model/common/dao/BaseDAO.java
+++ mockito_example/src/main/java/com/fs/humanResources/model/common/dao/BaseDAO.java
@@ -21,25 +21,25 @@ public abstract class BaseDAO<T> {
 
         queryString.append(entityClass.getSimpleName()).append(" o ");
 
-        final Query query = this.entityManager.createQuery(queryString.toString());
+        final Query query = entityManager.createQuery(queryString.toString());
 
         return (Long) query.getSingleResult();
     }
 
-    public T create(final T t) {
-        this.entityManager.persist(t);
-        return t;
+    public T create(final T entity) {
+        entityManager.persist(entity);
+        return entity;
     }
 
-    public void delete(final Object id) {
-        this.entityManager.remove(this.entityManager.getReference(entityClass, id));
+    public void delete(final T entity) {
+        entityManager.remove(entityManager.merge(entity));
     }
 
-    public T find(final Object id) {
-        return (T) this.entityManager.find(entityClass, id);
+    public T findById(final Object id) {
+        return (T) entityManager.find(entityClass, id);
     }
 
-    public T update(final T t) {
-        return this.entityManager.merge(t);
+    public T update(final T entity) {
+        return entityManager.merge(entity);
     }
 }
--- mockito_example/src/test/java/com/fs/humanResources/model/address/dao/AddressDAOTest.java
+++ mockito_example/src/test/java/com/fs/humanResources/model/address/dao/AddressDAOTest.java
@@ -1,6 +1,8 @@
 package com.fs.humanResources.model.employee.dao;
 
 import com.fs.common.BaseUnitTest;
+import com.fs.humanResources.model.address.dao.AddressDAO;
+import com.fs.humanResources.model.address.entities.Address;
 import com.fs.humanResources.model.employee.entities.Employee;
 import org.junit.Assert;
 import org.junit.Before;
@@ -12,17 +14,16 @@ import org.mockito.Mock;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 
-import static org.mockito.Matchers.anyObject;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-public class EmployeeDAOTest extends BaseUnitTest {
+public class AddressDAOTest extends BaseUnitTest {
 
     @InjectMocks
-    EmployeeDAO employeeDAO;
+    AddressDAO addressDAO;
 
     @Mock
     EntityManager entityManager;
@@ -42,38 +43,36 @@ public class EmployeeDAOTest extends BaseUnitTest {
 
     @Test
     public void countAll_uses_expected_query() {
-        Long actualCount = employeeDAO.countAll();
+        Long actualCount = addressDAO.countAll();
 
         Assert.assertEquals(expectedCount, actualCount);
 
-        verify(entityManager, times(1)).createQuery(eq("SELECT count(o) from Employee o "));
+        verify(entityManager, times(1)).createQuery(eq("SELECT count(o) from Address o "));
         verify(query,times(1)).getSingleResult();
     }
 
     @Test
     public void create_persits_expected_employee() {
-        Employee employee = new Employee();
-        employeeDAO.create(employee);
+        Address address = new Address();
+        addressDAO.create(address);
 
-        verify(entityManager, times(1)).persist(eq(employee));
+        verify(entityManager, times(1)).persist(eq(address));
     }
 
     @Test
     public void delete_removes_expected_employee() {
-        Employee employee = new Employee();
+        Address address = new Address();
+        addressDAO.delete(address);
 
-        employeeDAO.delete(employee);
-
-        verify(entityManager, times(1)).merge(eq(employee));
-        verify(entityManager, times(1)).remove(Matchers.<Employee>anyObject());
+        verify(entityManager, times(1)).merge(eq(address));
+        verify(entityManager, times(1)).remove(Matchers.<Address>anyObject());
     }
 
     @Test
     public void delete_merges_expected_employee() {
-        Employee employee = new Employee();
+        Address address = new Address();
+        addressDAO.update(address);
 
-        employeeDAO.update(employee);
-
-        verify(entityManager, times(1)).merge(eq(employee));
+        verify(entityManager, times(1)).merge(eq(address));
     }
 }
